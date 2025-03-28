2cf53cd36f5cac8fc6a105d2a6c883e727749d31
anggih <anggih.septiawan@wingscorp.com>
2025-03-28 10:45:45 +0700

    chore: update import style

--- components/alert/alert.tsx
+++ components/alert/alert.tsx
@@ -1,29 +1,23 @@
-import { Warning, Info } from "@mui/icons-material"
+import Warning from "@mui/icons-material/Warning"
+import Info from "@mui/icons-material/Info"
 import MuiAlert, { AlertProps as MUIAlertProps } from "@mui/material/Alert"
 
 declare module "@mui/material/Alert" {
-  interface AlertPropsVariantOverrides {
-    alert: true
-    info: true
-  }
+	interface AlertPropsVariantOverrides {
+		alert: true
+		info: true
+	}
 }
 
 export type AlertProps = MUIAlertProps & {
-  variant: keyof typeof iconVariant
+	variant: keyof typeof iconVariant
 }
 
 const iconVariant = {
-  alert: <Warning />,
-  info: <Info />,
+	alert: <Warning />,
+	info: <Info />
 }
 
 export const Alert = ({ variant = "alert", ref, ...props }: AlertProps) => {
-  return (
-    <MuiAlert
-      ref={ref}
-      variant={variant}
-      {...props}
-      icon={iconVariant[variant]}
-    />
-  )
+	return <MuiAlert ref={ref} variant={variant} {...props} icon={iconVariant[variant]} />
 }
